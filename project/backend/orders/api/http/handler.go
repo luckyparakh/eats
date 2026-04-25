fa8b7639b6c012db06016d2c41ca4d5add27de37
Rishi Parakh <rishi.parakh@prod.hclpnp.com>
2026-04-25 15:29:52 +0530

    first commit

---
+++ project/backend/orders/api/http/handler.go
@@ -0,0 +1,29 @@
+package http
+
+import (
+	"context"
+
+	"eats/backend/common"
+
+	"github.com/google/uuid"
+)
+
+type Handler struct{}
+
+func NewHandler() Handler {
+	return Handler{}
+}
+
+func Register(ctx context.Context, e common.EchoRouter, handler Handler) error {
+	RegisterHandlers(e, NewStrictHandler(&handler, nil))
+	return nil
+}
+
+func (h *Handler) RegisterCustomer(
+	ctx context.Context,
+	request RegisterCustomerRequestObject,
+) (RegisterCustomerResponseObject, error) {
+	return RegisterCustomer201JSONResponse{
+		CustomerUuid: uuid.New(),
+	}, nil
+}
